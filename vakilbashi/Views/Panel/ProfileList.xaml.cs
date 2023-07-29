ba70e7e2491b41d2495df3ead4f20f677aa3eef7
MechaRR <amir.fummechanic@yahoo.com>
2023-07-29 14:13:18 +0330

    profile list page

--- vakilbashi/Views/Panel/ProfileList.xaml.cs
+++ vakilbashi/Views/Panel/ProfileList.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace vakilbashi.Views.Panel;
 
 public partial class ProfileList : ContentPage
@@ -5,5 +7,11 @@ public partial class ProfileList : ContentPage
 	public ProfileList()
 	{
 		InitializeComponent();
+
+		NavigationPage.SetHasNavigationBar(this, false);
 	}
+    private async void back_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
